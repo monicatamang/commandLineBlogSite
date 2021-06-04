efbad9889fe7fe856ebd8745aa56b923a4b48d28
Monica <tamang.monica@gmail.com>
2021-06-04 14:11:20 -0600

    Fixed issues with the login functionality, working on adding escape characters to blog posts

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import dbcreds
 import mariadb
 import traceback
 
-def create_blog_post(username):
+def create_blog_post():
     try:
-        print(f"\nNew Blog Post by {username}")
+        print(f"\nNew Blog Post by @{username_login}")
         print("------------------------------ \n")
         blog_content = input("Enter text here: \n\n")
 
-        cursor.execute(f"INSERT INTO blog_post(username, content) VALUES('{username}', '{blog_content}')")
+        cursor.execute(f"INSERT INTO blog_post(username, content) VALUES('{username_login}', '{blog_content}')")
         conn.commit()
 
-        print(f"\n{username}'s blog post was successfully uploaded. \n")
+        print(f"\n@{username_login}'s blog post was successfully uploaded. \n")
     except:
         print("\nAn error occured. Failed to upload post. \n")
         traceback.print_exc()
@@ -34,13 +34,13 @@ def view_all_blog_posts():
         print("\nAn error has occured. Failed to view all posts. \n")
         traceback.print_exc()
 
-def get_selection(username):
+def get_selection():
     while(True):
         try:
             print("\nPlease select one of the two options: \n1: Write a new post \n2: See all other posts \n")
             selection = int(input("Please enter your selection: "))
             if(selection == 1):
-                create_blog_post(username)
+                create_blog_post()
             elif(selection == 2):
                 view_all_blog_posts()
             else:
@@ -56,7 +56,7 @@ def get_selection(username):
 def check_user_login():
     while(True):
         print("\nWelcome to the Blog Site! \n")
-        print("\nLogin")
+        print("Login")
         print("----- \n")
         username = input("Please enter your username: ")
         password = input("Please enter your password: ")
@@ -68,41 +68,41 @@ def check_user_login():
             for i in range (len(database_user_login_info)):
                 if(database_user_login_info[i][0] == username and database_user_login_info[i][1] == password):
                     print("\nYou have successfully logged in.")
-                    run_application(username)
-                    return True
+                    return database_user_login_info[i][0]
         except:
-            print(f"\nAn error has occured. Failed to retrieve {username}'s login information. \n")
+            print(f"\nAn error has occured. Failed to retrieve @{username}'s login information. \n")
             traceback.print_exc()
 
-def run_application(username):
-    get_selection(username)
-    check_user_exit = input("Would you like to exit the Blog Site? Y/N: ")
-    if(check_user_exit == "Y" or check_user_exit == "y"):
-        print("\nThank you for visiting the Blog Site!")
-    elif(check_user_exit == "N" or check_user_exit == "n"):
-        get_selection(username)
-    else:
-        print("\nInvalid selection.")    
+def run_application():
+    while(True):
+        check_user_exit = input("Would you like to exit the Blog Site? Y/N: ")
+        if(check_user_exit == "Y" or check_user_exit == "y"):
+            print("\nThank you for visiting the Blog Site!")
+            break
+        elif(check_user_exit == "N" or check_user_exit == "n"):
+            get_selection()
+        else:
+            print("\nInvalid selection.")    
 
 try:
     conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
     cursor = conn.cursor()
 
-    check_user_login()
+    username_login = check_user_login()
+    get_selection()
+    run_application()
 
 except:
     print("\nAn error in the database has occured. \n")
     traceback.print_exc()
 
 try:
-    print("Closing cursor.")
     cursor.close()
 except:
     print("\nAn error has occured. Failed to close cursor. \n")
     traceback.print_exc()
 
 try:
-    print("Closing connection.")
     conn.close()
 except:
     print("\nAn error has occured. Failed to close connection. \n")
